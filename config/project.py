6f203b11b451ae8cfa2f73ea56213e6aab304b06
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:55 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-go"
-project_long_description = "Demos for the go language"
-project_year_started = 2015
-project_description = """Demos for the go language"""
+description_short = "Demos for the go language"
